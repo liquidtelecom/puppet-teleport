232aef3fdf7bfc4170cd6d4746134aab217bb408
Anthony Somerset <anthony@sts.io>
2024-06-24 15:17:35 +0200

    we should restart if the symlink changes

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,9 +6,10 @@
 class teleport::service {
   if $teleport::manage_service == true and $teleport::init_style {
     service { 'teleport':
-      ensure   => $teleport::service_ensure,
-      enable   => $teleport::service_enable,
-      provider => $teleport::init_style,
+      ensure    => $teleport::service_ensure,
+      enable    => $teleport::service_enable,
+      provider  => $teleport::init_style,
+      subscribe => File["${teleport::bin_dir}/teleport"],
     }
   }
 }
